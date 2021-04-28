a45d4ac8e7735f0f8fd30779e5aec8eaaf8734e0
s32989 <s32989@gmail.com>
2021-04-28 19:41:36 -0400

    renaming

---
+++ app/src/test/java/com/bakecalc/app/RestAPITests.java
@@ -0,0 +1,13 @@
+package com.bakecalc.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAPITests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
